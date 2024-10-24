7b642b34cf81683e225c876a16acc1ae3e116500
ubaid1840 <107393880+ubaid1840@users.noreply.github.com>
2024-10-24 16:50:59 +0500

    admin auto route set

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,8 +1,11 @@
 "use client"
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 
 export default function Page () {
     const router = useRouter()
+   useEffect(()=>{
     router.push("/admin/usermanagement")
+   },[])
 }
