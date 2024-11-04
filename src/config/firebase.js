e4cba01c521ef16d15ed111dbeb4ad73f59eac01
ubaid1840 <107393880+ubaid1840@users.noreply.github.com>
2024-11-05 02:07:59 +0500

    .env.example updated

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getStorage} from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth  = getAuth(app)
+const provider = new GoogleAuthProvider();
+const storage = getStorage(app)
+
+export {auth, provider, app, storage}
